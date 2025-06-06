607f29e9d4d8c35560c450798b39215c60194eef
Ye0078 <70047423+Ye0078@users.noreply.github.com>
2025-06-06 23:47:58 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 from telegram.ext import ApplicationBuilder, CommandHandler
 
-TOKEN = os.getenv("7981257271:AAGWpkX-FnxsObUMnMET82HtRVdBNrBI4Ro")
+TOKEN = os.getenv("7572057562:AAEYXsvfSGUlD0iyL27LDNlQjlpeVS4hZ1E")
 
 async def start(update, context):
     await update.message.reply_text("မင်္ဂလာပါ။ Bot သက်တမ်းဝင်နေပါတယ်။")
